c64fd4ba64720c612e73a4d675a58059df1d528a
silvio usai <berzylius@hotmail.it>
2021-05-18 18:02:53 +0200

    fine

--- snack2/index.php
+++ snack2/index.php
@@ -19,7 +19,7 @@ $risultato = 'Accesso negato';
 
 if( strlen($name) > 3  ) {
 
-    if( strpos($email, '@') === true && strpos($email, '.') === true )  {
+    if( strpos($email, '@') >= 0 && strpos($email, '.') >= 0 )  {
 
         if( is_numeric($age) ) {
 
@@ -31,18 +31,3 @@ if( strlen($name) > 3  ) {
 echo $risultato;
 ?>
 
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
