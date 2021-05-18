6cbf1ca843b5492b5ecfc9a14cc6d5f7f8bdc4c7
silvio usai <berzylius@hotmail.it>
2021-05-18 17:32:42 +0200

    problema

---
+++ snack2/index.php
@@ -0,0 +1,48 @@
+<!-- Passare come parametri GET name, mail e age e verificare 
+(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
+che mail contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+
+
+<?php
+
+$email = $_GET['email'];
+$name = $_GET['name'];
+$age = $_GET['age'];
+
+var_dump($email);
+var_dump($name);
+var_dump($age);
+
+$risultato = 'Accesso negato';
+
+if( strlen($name) > 3  ) {
+
+    if( strpos($email, '@') === true && strpos($email, '.') === true )  {
+
+        if( is_numeric($age) ) {
+
+            $risultato = 'Accesso riuscito';            
+        }
+    } 
+}
+
+echo $risultato;
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
